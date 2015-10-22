71a2ca1e7dfcf08f3b38fe1f5df8c117b309f0e3
Ivan Wills <Ivan.Wills2@optus.com.au>
2015-10-23 09:39:52 +1100

    Getting components to be generated

--- generators/component/index.js
+++ generators/component/index.js
@@ -4,43 +4,75 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 
 module.exports = yeoman.generators.Base.extend({
-  prompting: function () {
-    var done = this.async();
+	prompting: function () {
+		var done = this.async();
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the grand ' + chalk.red('GeneratorRactiveFoundation') + ' generator!'
-    ));
+		// Have Yeoman greet the user.
+		this.log(yosay(
+			'Welcome to the grand ' + chalk.red('RF component') + ' generator!'
+		));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'componentDir',
-      message: 'Directory to store components?',
-      default: 'src/components'
-    }];
+		var prompts = [{
+			type: 'text',
+			name: 'componentDir',
+			message: 'Directory to store components?',
+			default: 'src/components'
+		}, {
+			type: 'text',
+			name: 'componentName',
+			message: 'Name of the component?'
+		}];
 
-    this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someOption;
+		this.prompt(prompts, function (props) {
+			this.props = props;
+			// To access props later use this.props.someOption;
 
-      done();
-    }.bind(this));
-  },
+			done();
+		}.bind(this));
+	},
 
-  writing: {
-    app: function () {
-      this.fs.copy(
-        this.templatePath('_package.json'),
-        this.destinationPath('package.json')
-      );
-      this.fs.copy(
-        this.templatePath('_bower.json'),
-        this.destinationPath(this.props.componentDir + '/bower.json')
-      );
-    },
-  },
+	writing: {
+		app: function () {
+			this.fs.copy(
+				this.templatePath('wingComponent.scss'),
+				this.destinationPath(this.props.componentDir + '/' + this.props.componentName + '/' + this.props.componentName + '.scss')
+			);
+			this.fs.copy(
+				this.templatePath('use-cases/customAtributes.json'),
+				this.destinationPath(this.props.componentDir + '/' + this.props.componentName + '/use-cases/customAtributes.json')
+			);
+			this.fs.copy(
+				this.templatePath('use-cases/dataDriven.json'),
+				this.destinationPath(this.props.componentDir + '/' + this.props.componentName + '/use-cases/dataDriven.json')
+			);
+			this.fs.copy(
+				this.templatePath('manifest.json'),
+				this.destinationPath(this.props.componentDir + '/' + this.props.componentName + '/manifest.json')
+			);
+			this.fs.copy(
+				this.templatePath('wingComponent.hbs'),
+				this.destinationPath(this.props.componentDir + '/' + this.props.componentName + '/' + this.props.componentName + '.hbs')
+			);
+			this.fs.copy(
+				this.templatePath('README.md'),
+				this.destinationPath(this.props.componentDir + '/' + this.props.componentName + '/README.md')
+			);
+			this.fs.copy(
+				this.templatePath('wingComponent.js'),
+				this.destinationPath(this.props.componentDir + '/' + this.props.componentName + '/' + this.props.componentName + '.js')
+			);
+			this.fs.copy(
+				this.templatePath('wingComponent.feature'),
+				this.destinationPath(this.props.componentDir + '/' + this.props.componentName + '/' + this.props.componentName + '.feature')
+			);
+			this.fs.copy(
+				this.templatePath('wingComponent.steps.js'),
+				this.destinationPath(this.props.componentDir + '/' + this.props.componentName + '/' + this.props.componentName + '.steps.js')
+			);
+		},
+	},
 
-  install: function () {
-    //this.installDependencies();
-  }
+	install: function () {
+		//this.installDependencies();
+	}
 });
