cdd6ddfd8e4b73f05c1b58259fbffb97d9fc9259
Ivan Wills <ivan.wills@gmail.com>
2015-10-26 06:56:56 +1100

    Changing templates to be options

--- generators/component/index.js
+++ generators/component/index.js
@@ -4,6 +4,20 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 
 module.exports = yeoman.generators.Base.extend({
+	templates: {
+		'{templatePath}/wingComponent.scss'            : '{componentName}.scss',
+		'{templatePath}/use-cases/customAtributes.json': 'use-cases/customAtributes.json',
+		'{templatePath}/use-cases/dataDriven.json'     : 'use-cases/dataDriven.json',
+		'{templatePath}/use-cases/templateDriven.json' : 'use-cases/templateDriven.json',
+		'{templatePath}/use-cases/templateDriven.hbs'  : 'use-cases/templateDriven.hbs',
+		'{templatePath}/manifest.json'                 : 'manifest.json',
+		'{templatePath}/wingComponent.hbs'             : '{componentName}.hbs',
+		'{templatePath}/README.md'                     : 'README.md',
+		'{templatePath}/wingComponent.js'              : '{componentName}.js',
+		'{templatePath}/wingComponent.feature'         : '{componentName}.feature',
+		'{templatePath}/wingComponent.steps.js'        : '{componentName}.steps.js'
+	},
+
 	prompting: function () {
 		var done = this.async();
 
@@ -39,53 +53,21 @@ module.exports = yeoman.generators.Base.extend({
 		app: function () {
 			var componentDir  = this.props.componentDir,
 				componentName = this.props.componentName,
+				templatePath  = this.sourceRoot(),
 				basePath      = componentDir + '/' + componentName + '/';
 
-			this.fs.copyTpl(
-				this.templatePath('wingComponent.scss'),
-				this.destinationPath(basePath + componentName + '.scss'),
-				this.props
-			);
-			this.fs.copyTpl(
-				this.templatePath('use-cases/customAtributes.json'),
-				this.destinationPath(basePath + 'use-cases/customAtributes.json'),
-				this.props
-			);
-			this.fs.copyTpl(
-				this.templatePath('use-cases/dataDriven.json'),
-				this.destinationPath(basePath + 'use-cases/dataDriven.json'),
-				this.props
-			);
-			this.fs.copyTpl(
-				this.templatePath('manifest.json'),
-				this.destinationPath(basePath + 'manifest.json'),
-				this.props
-			);
-			this.fs.copyTpl(
-				this.templatePath('wingComponent.hbs'),
-				this.destinationPath(basePath + componentName + '.hbs'),
-				this.props
-			);
-			this.fs.copyTpl(
-				this.templatePath('README.md'),
-				this.destinationPath(basePath + 'README.md'),
-				this.props
-			);
-			this.fs.copyTpl(
-				this.templatePath('wingComponent.js'),
-				this.destinationPath(basePath + componentName + '.js'),
-				this.props
-			);
-			this.fs.copyTpl(
-				this.templatePath('wingComponent.feature'),
-				this.destinationPath(basePath + componentName + '.feature'),
-				this.props
-			);
-			this.fs.copyTpl(
-				this.templatePath('wingComponent.steps.js'),
-				this.destinationPath(basePath + componentName + '.steps.js'),
-				this.props
-			);
+			for (var template in this.templates) {
+				var dest = this.templates[template];
+				template = template.replace(/[{]templatePath[}]/g, templatePath);
+				template = template.replace(/[{]componentName[}]/g, componentName);
+				dest = dest.replace(/[{]componentName[}]/g, componentName);
+
+				this.fs.copyTpl(
+					template,
+					this.destinationPath(basePath + '/' + dest),
+					this.props
+				);
+			}
 		},
 	},
 
